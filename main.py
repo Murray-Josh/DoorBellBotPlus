18b2cf2516c297efb2b1f8420963853428cd1a1c
Alfie Richardson <53946532+AlfieRichardson@users.noreply.github.com>
2020-12-17 22:12:01 +0000

    Renamed checkWasher to check_washer to make functions consistent

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ import time
 client = discord.Client()
 BOT_TOKEN = "NzU4NDE5NzgyOTYxMzMyMjc1.X2urdw.RyvbayO1wgvnTeOZ78mHYMGBQAs"
 global washingMachine
-global dryer 
+global dryer
 global washingMachineUser
 
 washingMachine = False
 washingMachineUser = ""
 dryer = False
 
-def checkWasher():
+
+def check_washer():
     global t0
-    if washingMachine == True:
-        return "No, "+ str(washingMachineUser) + " is using it, " + str(round((2700 - (time.time() - t0))/60, 1)) + "mins left"
+    if washingMachine:
+        return "No, " + str(washingMachineUser) + " is using it, " + str(
+            round((2700 - (time.time() - t0)) / 60, 1)) + "mins left"
     else:
         return 'Yes :)'
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    global washingMachine 
-    global washingMachineUser 
+    global washingMachine
+    global washingMachineUser
     global t0
 
     if message.author == client.user:
@@ -38,23 +42,22 @@ async def on_message(message):
     if message.content.startswith('!dontdead'):
         await message.channel.send('open inside')
 
-    if message.content.startswith('!dontopen') :
+    if message.content.startswith('!dontopen'):
         await message.channel.send('dead inside')
-
 
     if message.content.startswith("!use"):
         await message.channel.send('Done')
         washingMachine = True
-        #print(message.author)
-        washingMachineUser= message.author
+        # print(message.author)
+        washingMachineUser = message.author
         t0 = time.time()
-    
+
     if message.content.startswith("!free"):
-        await message.channel.send(checkWasher())
+        await message.channel.send(check_washer())
 
     if message.content.startswith("!finish"):
         await message.channel.send('Done')
         washingMachine = False
-        
-        
+
+
 client.run(BOT_TOKEN)
