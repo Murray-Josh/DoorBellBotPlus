91a3607c74e58e6f7a41b20b830f1149c77a4677
Joshua Murray <j.murray567@gmail.com>
2020-12-17 22:25:14 +0000

    99 quotes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import discord
 import os
 import time
+import random
+from discord.ext import commands
 
 client = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
 BOT_TOKEN = "NzU4NDE5NzgyOTYxMzMyMjc1.X2urdw.RyvbayO1wgvnTeOZ78mHYMGBQAs"
 global washingMachine
 global dryer 
@@ -22,6 +27,7 @@ def checkWasher():
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    #doorBell()
 
 @client.event
 async def on_message(message):
@@ -41,6 +47,8 @@ async def on_message(message):
     if message.content.startswith('!dontopen') :
         await message.channel.send('dead inside')
 
+    if message.content.startswith('!castles') :
+        await message.channel.send('Are Cool!')
 
     if message.content.startswith("!use"):
         await message.channel.send('Done')
@@ -50,11 +58,32 @@ async def on_message(message):
         t0 = time.time()
     
     if message.content.startswith("!free"):
+        newTest()
         await message.channel.send(checkWasher())
 
     if message.content.startswith("!finish"):
         await message.channel.send('Done')
         washingMachine = False
-        
-        
-client.run(BOT_TOKEN)
+
+    #if message.content.startswith("!ring"):
+    #    doorBell(ctx)
+
+#@bot.command()
+#async def doorBell(ctx):
+#    await ctx.send('DING DONG')
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(BOT_TOKEN)
