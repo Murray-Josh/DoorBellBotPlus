4379640bdaee4176fc978dedc97137e587a69fad
Joshua Murray <j.murray567@gmail.com>
2020-12-19 19:09:06 +0000

    Removed printer from misc

--- misc.py
+++ misc.py
@@ -7,11 +7,7 @@ class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.index = 0
-        self.printer.start()
         # bot.bg_task = bot.loop.create_task(Miscellaneous.my_background_task())
-
-    def cog_unload(self):
-        self.printer.cancel()
 
     @commands.command(name='dontdead')
     async def dont_dead(self, ctx):
