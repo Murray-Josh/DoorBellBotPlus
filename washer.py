8a11ed1e55d450c06fcb87a53c507175773dd3c4
Joshua Murray <j.murray567@gmail.com>
2020-12-20 02:11:59 +0000

    @s people when washing done

--- washer.py
+++ washer.py
@@ -1,27 +1,30 @@
 import time
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 
 class Washing(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.washingMachineUser = ''
+        self.washing_machine_user = ''
         self.washingMachine = False
         self.washTime = 0
+        self.washer_time_to_complete = 10
 
     @commands.command(name='use')
     async def use_washer(self, ctx):
         self.washingMachine = True
-        self.washingMachineUser = ctx.author
+        self.washing_machine_user = ctx.author
         self.washTime = time.time()
+        self.is_washer_done.start()
+
         await ctx.send('Done')
 
     @commands.command(name='free')
     async def free_washer(self, ctx):
         if self.washingMachine:
-            await ctx.send("No, " + str(self.washingMachineUser) + " is using it, " + str(
-                round((2700 - (time.time() - self.washTime)) / 60, 1)) + "minutes left")
+            await ctx.send("No, " + str(self.washing_machine_user) + " is using it, " + 
+            str(round((self.washer_time_to_complete - (time.time() - self.washTime)) / 60, 1)) + "minutes left")
         else:
             await ctx.send('Yes :)')
 
@@ -35,5 +38,17 @@ class Washing(commands.Cog):
         await ctx.send('Done')
 
 
+    @tasks.loop(seconds = 5)
+    async def is_washer_done(self):
+        if round((self.washer_time_to_complete - (time.time() - self.washTime)) / 60, 1) <= 0:
+            channel = self.bot.get_channel(int(789226620821045278))
+
+            await channel.send(self.washing_machine_user.mention 
+            + " your washing is done")
+            self.is_washer_done.stop()
+            self.washingMachine = False
+
+
+            
 def setup(bot):
     bot.add_cog(Washing(bot))
