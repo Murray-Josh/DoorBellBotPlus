0e05e856ba47efc174d88b13ff3d1c984111ba45
joshua <j.murray567@gmail.com>
2021-02-08 12:46:32 +0000

    Added renaming times

--- misc.py
+++ misc.py
@@ -1,6 +1,6 @@
 from discord.ext import tasks, commands
 from tictactoe import print_board
-
+import fileinput
 
 class Miscellaneous(commands.Cog):
 
@@ -21,6 +21,28 @@ class Miscellaneous(commands.Cog):
     async def play_tic_tac_toe(self, ctx):
         board = print_board()
         await ctx.send(board)
+
+    @commands.command(name='setring')
+    async def set_ring(self, ctx,arg, arg2):
+        ftime = open("SwanData.txt.txt", "r")
+        found = False
+        lineNum = 0
+        listOfLines = []
+        for line in ftime:
+            listOfLines.append(line)
+            lineNum += 1
+            values = line.split()
+            if values[0] == arg:
+                found = True
+                listOfLines[lineNum] = str(values[0] +" " +arg2)
+        if found:
+            ftime.writelines(listOfLines)
+            ftime.close()
+            await ctx.send("Found and renamed")
+
+        else:
+            await ctx.send("Time not found :(")
+
 
 
 def setup(bot):
